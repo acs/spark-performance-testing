10059b758c370283d6615774d1cb2039c311b147
Alvaro del Castillo <alvarodelcastillo@gmail.com>
2019-04-15 22:56:48 +0200

    Add some RDD samples as an starting point

--- src/main/scala/personal/acs/spark/PerfSparkRDD.scala
+++ src/main/scala/personal/acs/spark/PerfSparkRDD.scala
@@ -1,8 +1,40 @@
 package personal.acs.spark
 
+import org.apache.spark.sql.SparkSession
+
 object PerfSparkRDD {
+
+  def aggregations(spark:SparkSession): Unit = {
+    val words = Array("one", "two", "two", "three", "three", "three")
+    val wordPairsRDD = spark.sparkContext.parallelize(words).map(word => (word, 1))
+
+    val wordCountsWithReduce = wordPairsRDD
+      .reduceByKey((a, b) => a + b)
+      .collect()
+
+    // The performance in this operation is pretty bad
+    // https://databricks.gitbooks.io/databricks-spark-knowledge-base/content/best_practices/prefer_reducebykey_over_groupbykey.html
+    val wordCountsWithGroup = wordPairsRDD
+      .groupByKey()
+      .map(t => (t._1, t._2.sum))
+      .collect()
+
+    val res = 0
+  }
+
+  def join(spark:SparkSession): Unit = {
+    // let's play with zip tha join two dataframes
+  }
 
   def main(args: Array[String]) {
     println("Testing the performance in SparkRDD")
+
+    val spark = SparkSession.builder()
+      .appName("Spark performance testing")
+      .master("local[*]")
+      .getOrCreate()
+
+    aggregations(spark)
+
   }
 }
