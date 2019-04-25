35cb27d0671ccbec9f1535a6c06142726f619f74
Alvaro del Castillo <alvarodelcastillo@gmail.com>
2019-04-25 12:30:03 +0200

    Add more samples on howto use the Delta module

--- src/main/scala/personal/acs/spark/PerfSparkDelta.scala
+++ src/main/scala/personal/acs/spark/PerfSparkDelta.scala
@@ -12,8 +12,14 @@ object PerfSparkDelta {
       .master("local[*]")
       .getOrCreate()
 
-    val data = spark.range(0, 50000)
-    data.write.format("delta").mode(SaveMode.Overwrite)save("/tmp/delta-table")
 
+    spark.range(0, 10).write.format("delta").mode(SaveMode.Overwrite)save("/tmp/delta-table")
+    spark.range(11, 20).write.format("delta").mode(SaveMode.Overwrite)save("/tmp/delta-table")
+    spark.range(21, 30).write.format("delta").mode(SaveMode.Overwrite)save("/tmp/delta-table")
+
+    spark.read.format("delta").load("/tmp/delta-table").show()
+    spark.read.format("delta").option("versionAsOf", 0).load("/tmp/delta-table").show()
+    spark.read.format("delta").option("versionAsOf", 1).load("/tmp/delta-table").show()
+    spark.read.format("delta").option("versionAsOf", 2).load("/tmp/delta-table").show()
   }
 }
